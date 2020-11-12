3bd5c7820d77872ecd50722c454a765d24ab8a9a
lhsthree <lhsthree@gmail.com>
2020-11-11 18:58:03 -0600

    chore: updating about

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <section id="about">
 	        	<p className="about_info">
-	            <strong>A bunch ofdsdsfadsdf goddamm degenerates.</strong><br />
+	            <strong>A bunch of goddamm degenerates.</strong><br />
 	            This fast talking, gum chewing quartet hails from Chicago, Illinois. Founded in 2011, they have evolved an effect driving sound of gutter rock meets space punk noise that can be found at any of their chaotic displays of depravity. Not being strangers to trouble, they have been put through the ringer, having lost it all just to come back stronger and stranger, bringing with them their praise for John Blutarsky. These basement bar dwelling mid western animals can be found in the closest dumpster near you.
 	        	</p>
 		</section>
