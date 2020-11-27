0e81d1fd4e3f0fffc2673a74570311a9197ae2c9
lhsthree <lhsthree@gmail.com>
2020-11-26 21:09:38 -0600

    feat: add bandcamp player

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,19 +1,12 @@
 import React from 'react'
+import Iframe from 'react-iframe'
 
 export default function Home() {
     return (
         <section className="music">
-        <div className="music"><p>Shred Forever</p>
-		    <audio src="audio/shred_forever.mp3" controls>
-		    <embed
-		        src="audio/shred_forever.mp3"
-		        width="300"
-		        height="90"
-		        loop={false}
-		        autostart="false" />
-		    </audio>
+        <div className="music">
+        <iframe src="https://bandcamp.com/EmbeddedPlayer/album=4255692830/size=small/bgcol=333333/linkcol=9a64ff/track=1915559220/transparent=true/" seamless><a href="https://butchered.bandcamp.com/album/wax-pathetic">Wax Pathetic by Butchered</a></iframe>
     	</div>
-
 		<img src="images/main_image.jpg" />
 		</section>
     )
