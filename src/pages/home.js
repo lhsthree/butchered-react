3beab31df0e233c3c6303719ad6bff37f5661ed3
lhsthree <lhsthree@gmail.com>
2020-11-30 17:36:23 -0600

    fix: error with iframe not being centered. readded div.

--- src/pages/home.js
+++ src/pages/home.js
@@ -4,8 +4,10 @@ import Iframe from 'react-iframe'
 export default function Home() {
     return (
         <section className="music">
-	        <iframe src="https://bandcamp.com/EmbeddedPlayer/album=4255692830/size=large/bgcol=333333/linkcol=CD48DC/tracklist=false/artwork=small/track=1915559220/transparent=true/" seamless><a href="https://butchered.bandcamp.com/album/wax-pathetic">Wax Pathetic by Butchered</a></iframe>
-			<img src="images/main_image.jpg" />
+        <div className="music">
+ 			<iframe src="https://bandcamp.com/EmbeddedPlayer/album=4255692830/size=large/bgcol=333333/linkcol=CD48DC/tracklist=false/artwork=small/track=1915559220/transparent=true/" seamless><a href="https://butchered.bandcamp.com/album/wax-pathetic">Wax Pathetic by Butchered</a></iframe>    	
+ 		</div>
+		<img src="images/main_image.jpg" />
 		</section>
     )
 }
